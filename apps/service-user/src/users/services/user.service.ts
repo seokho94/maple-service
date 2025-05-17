0153bf4183ab87115c7d9783e1fa2d35cc4d3260
seokho94 <32010878+seokho94@users.noreply.github.com>
2025-05-17 22:38:05 +0900

    [add][user-service][service] 사용자 정보 CRUD 추가

--- apps/service-user/src/users/services/user.service.ts
+++ apps/service-user/src/users/services/user.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, ConflictException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 import { UserInfo, UserInfoSchema } from '../entities/userInfo.schema';
 
 @Injectable()
@@ -10,14 +11,60 @@ export class UserService {
     @InjectModel(UserInfo.name) private readonly userModel: Model<UserInfoSchema>,
   ) {}
 
-  async getUser(): Promise<string> {
+  testCode(): string {
     return 'Hello World!';
   }
 
-  async create(createUserDto: CreateUserDto): Promise<UserInfo> {
-    console.log(`create_user in user: ${JSON.stringify(createUserDto, null, 2)}`);
-    const createUser = new this.userModel(createUserDto);
-    console.log(`userModel in user: ${JSON.stringify(createUserDto, null, 2)}`);
-    return await createUser.save();
+  //전체 조회
+  async findAll(): Promise<UserInfo[]> {
+    return await this.userModel.find().exec();
+  }
+
+  //단일 조회
+  async findOne(id: string): Promise<UserInfo> {
+    const user = await this.userModel.findById(id).exec();
+    if (!user) {
+      throw new NotFoundException('사용자를 찾을 수 없습니다');
+    }
+    return user;
+  }
+
+  //생성
+  async create(createUserDto: CreateUserDto): Promise<{ message: string }> {
+    try {
+      const createUser = new this.userModel(createUserDto);
+      await createUser.save();
+      return { message: '사용자 생성 완료' };
+    } catch (error) {
+      if (error.code === 11000) {
+        const key = Object.keys(error.keyPattern)[0];
+        throw new ConflictException(`사용자가 이미 존재합니다`);
+      }
+      throw error;
+    }
+  }
+
+  //사용자 정보 수정
+  async update(id: string, updateUserDto: UpdateUserDto): Promise<{ message: string }> {
+    try {
+      const updatedUser = await this.userModel
+        .findByIdAndUpdate(id, updateUserDto, { new: true })
+        .exec();
+      if (!updatedUser) throw new NotFoundException('사용자를 찾을 수 없습니다');
+      return { message: '사용자 정보 수정 완료' };
+    } catch (error) {
+      if (error.code === 11000) {
+        const key = Object.keys(error.keyPattern)[0];
+        throw new ConflictException('변경할 수 없습니다.');
+      }
+      throw error;
+    }
+  }
+
+  //사용자 삭제
+  async remove(id: string): Promise<{ message: string }> {
+    const result = await this.userModel.findByIdAndDelete(id).exec();
+    if (!result) throw new NotFoundException('사용자를 찾을 수 없습니다');
+    return { message: '사용자 삭제 완료' };
   }
 }
