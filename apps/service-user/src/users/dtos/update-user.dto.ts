77d3e0bc2c0ec20d179e1e5fd85d21b9c67583f1
seokho94 <32010878+seokho94@users.noreply.github.com>
2025-05-17 22:36:43 +0900

    [mod] update-user 추가
    update-user DTO 추가

---
+++ apps/service-user/src/users/dtos/update-user.dto.ts
@@ -0,0 +1,30 @@
+import { IsOptional, IsString, IsEmail, IsMobilePhone, MinLength } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString()
+  @MinLength(3)
+  userName?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(6)
+  password?: string;
+
+  @IsOptional()
+  @IsString()
+  nickName?: string;
+  
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsString()
+  @IsMobilePhone('ko-KR')
+  phoneNumber?: string;
+
+  @IsOptional()
+  @IsString()
+  role?: string;
+}
