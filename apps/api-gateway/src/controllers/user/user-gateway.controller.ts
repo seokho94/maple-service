a2a42d05c8b7424f3614b9f9d987e998e3040248
seokho94 <32010878+seokho94@users.noreply.github.com>
2025-05-17 22:37:27 +0900

    [add] 사용자 Controller CRUD 추가

--- apps/api-gateway/src/controllers/user/user-gateway.controller.ts
+++ apps/api-gateway/src/controllers/user/user-gateway.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Post, Inject, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Patch, Delete, Inject, Param, Body, Query } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { CreateUserDto } from '../../dtos/user/create-user.dto';
+import { UpdateUserDto } from '../../dtos/user/update-user.dto';
 import { Observable } from 'rxjs';
 
 
@@ -10,14 +11,39 @@ export class UserGatewayController {
     @Inject('SERVICE_USER') private readonly serviceUserClient: ClientProxy,
   ) {}
 
+  @Get('/test')
+  async getTestCode() {
+    return this.serviceUserClient.send('test_code', {});
+  }
+
   @Get()
-  async getUser() {
-    return this.serviceUserClient.send('get_users', {});
+  async findAll() {
+    return this.serviceUserClient.send('get_all_users', {});
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.serviceUserClient.send('get_user', { id });
   }
 
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
     console.log('create_user', createUserDto);
     return this.serviceUserClient.send('create_user', createUserDto);
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.serviceUserClient.send('update_user', {id, updateUserDto });
+  }
+
+  @Delete(':id')
+  async remove(
+    @Param('id') id: string,
+  ) {
+    return this.serviceUserClient.send('remove_user', id);
   }
 }
--- apps/service-user/src/users/controllers/service-user.controller.ts
+++ apps/service-user/src/users/controllers/service-user.controller.ts
@@ -1,20 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { UpdateUserDto } from '../dtos/update-user.dto';
 
 @Controller()
 export class ServiceUserController {
   constructor(private readonly userService: UserService) {}
 
-  @MessagePattern('get_users')
-  async getUsers() {
-    return this.userService.getUser();
+  // 테스트 코드
+  @MessagePattern('test_code')
+  testCode() {
+    return this.userService.testCode();
   }
 
+  // 전체 사용자 조회
+  @MessagePattern('get_all_users')
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  // 단일 사용자 조회
+  @MessagePattern('get_user')
+  async findOne(@Payload() data: { id: string }) {
+    return this.userService.findOne(data.id);
+  }
+
+  // 사용자 생성
   @MessagePattern('create_user')
-  async createUser(createUserDto: CreateUserDto) {
-    console.log(`create_user: ${JSON.stringify(createUserDto)}`);
+  async createUser(@Payload() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
+  }
+
+  // 사용자 정보 수정
+  @MessagePattern('update_user')
+  async update(@Payload() data: { id: string; updateUserDto: UpdateUserDto }) {
+    return this.userService.update(data.id, data.updateUserDto);
+  }
+
+  // 사용자 삭제
+  @MessagePattern('remove_user')
+  async remove(@Payload() data: { id: string }) {
+    return this.userService.remove(data.id);
   }
 }
